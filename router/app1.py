2fc1f388f2c92963acb210b33b3c96b9059ca847
amankumar739728 <aman.kumar739728@gmail.com>
2023-09-14 17:52:35 +0530

    updated app1.py file with run command at last

--- router/app1.py
+++ router/app1.py
@@ -195,4 +195,4 @@ async def profile(request: Request, username: str = Cookie(default=None)):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run("app1:app", host="127.0.0.1", port=8000,reload=True)
